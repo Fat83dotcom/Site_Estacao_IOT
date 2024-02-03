bc216e314109ff7260451eb7dd78bb00da38f51d
fat83dotcom <fernando.quimic@gmail.com>
2024-02-03 18:58:07 -0300

    Modelo de dados.

--- Core/models.py
+++ Core/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class Localization(models.Model):
+    id_loc = models.AutoField(primary_key=True)
+    city = models.CharField(max_length=100)
+    neighborhood = models.CharField(max_length=100)
+    coordinate = models.CharField(
+        max_length=100, null=True, blank=True, unique=True
+    )
+
+
+class Sensor(models.Model):
+    id_sen = models.AutoField(primary_key=True)
+    mac = models.CharField(max_length=20, unique=True, null=False)
+    id_localization = models.ForeignKey(
+        'Localization', on_delete=models.SET_NULL, null=True
+    )
+
+
+class DataSensor(models.Model):
+    id_data_sensor = models.AutoField(primary_key=True)
+    date_hour = models.DateTimeField()
+    temperature = models.FloatField()
+    humidity = models.FloatField()
+    pressure = models.FloatField()
+    id_sensor = models.ForeignKey(
+        'Sensor', on_delete=models.CASCADE, null=False
+    )
