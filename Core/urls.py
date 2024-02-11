749c4aabec3b2ea5556f5c1d576dcd4801415836
fat83dotcom <fernando.quimic@gmail.com>
2024-02-11 19:30:13 -0300

    Caminhos para páginas e arquivos de media.

--- Core/urls.py
+++ Core/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from Core.views import Index, SensorView
 
 
 urlpatterns = [
-    path('', Index.as_view(), 'index'),
-    path('sensor/', SensorView.as_view(), 'sensor'),
+    path('', Index.as_view(), name='index'),
+    path('sensor/', SensorView.as_view(), name='sensor'),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+print(urlpatterns)
