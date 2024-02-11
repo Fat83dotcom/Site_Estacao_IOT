80f962d205a88f7592800cbc169e1e0588bf8ab4
fat83dotcom <fernando.quimic@gmail.com>
2024-02-11 19:31:13 -0300

    View para a página principal.

--- Core/views.py
+++ Core/views.py
@@ -1,9 +1,21 @@
+from typing import Any
+from django.db.models.query import QuerySet
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.views.generic import View, ListView
+from Core.models import Sensor
 
 
-class Index():
-    pass
+class Index(View):
+    template = 'index/index.html'
+
+    def get(self, request) -> HttpResponse:
+        query = Sensor.objects.all()
+        context = {
+            'sensors': query
+        }
+        return render(request, self.template, context)
 
 
-class SensorView():
+class SensorView(View):
     pass
