1ef132eea8838d86d6ec2350508bd843c868984b
fat83dotcom <fernando.quimic@gmail.com>
2024-02-11 23:45:48 -0300

    Path para about view.

--- Core/urls.py
+++ Core/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from Core.views import Index, SensorView
+from Core.views import Index, SensorView, AboutView
 
 
 urlpatterns = [
     path('', Index.as_view(), name='index'),
     path('sensor/', SensorView.as_view(), name='sensor'),
+    path('about/', AboutView.as_view(), name='about'),
 ]
+
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 print(urlpatterns)
