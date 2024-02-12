630d0066e4719531c1bc23b750776ca2a3e1669d
fat83dotcom <fernando.quimic@gmail.com>
2024-02-11 23:50:43 -0300

    Desenvolvimento das views.

--- Core/views.py
+++ Core/views.py
@@ -1,9 +1,11 @@
-from typing import Any
-from django.db.models.query import QuerySet
-from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
-from django.views.generic import View, ListView
-from Core.models import Sensor
+import datetime
+from django.db.models import Q
+from datetime import timedelta
+from django.utils import timezone
+from django.http import HttpResponse
+from django.views.generic import View
+from Core.models import Sensor, DataSensor
+from django.shortcuts import render, redirect
 
 
 class Index(View):
@@ -18,4 +20,50 @@ class Index(View):
 
 
 class SensorView(View):
-    pass
+    template = 'sensor/sensors.html'
+
+    def post(self, request) -> HttpResponse:
+        idSensor = request.POST.get('selectSensor')
+        if 'default' in idSensor:
+            return redirect(to='index')
+        dataSensor24hrs = DataSensor.objects.filter(
+            Q(id_sensor=idSensor) & Q(
+                date_hour__range=(
+                    self.timeLast(24), self.timeNow()
+                )
+            )
+        )
+        dataSensor168hrs = DataSensor.objects.filter(
+            Q(id_sensor=idSensor) & Q(
+                date_hour__range=(
+                    self.timeLast(168), self.timeNow()
+                )
+            )
+        )
+        dataSensor720rs = DataSensor.objects.filter(
+            Q(id_sensor=idSensor) & Q(
+                date_hour__range=(
+                    self.timeLast(720), self.timeNow()
+                )
+            )
+        )
+        context = {
+            'sensors': dataSensor24hrs,
+        }
+        return render(request, self.template, context)
+
+    def get(self, request) -> HttpResponse:
+        return redirect(to='index')
+
+    def timeNow(self) -> datetime:
+        return timezone.now()
+
+    def timeLast(self, hoursAgo) -> datetime:
+        return self.timeNow() - timedelta(hours=hoursAgo)
+
+
+class AboutView(View):
+    template = 'about/about.html'
+
+    def get(self, request) -> HttpResponse:
+        return render(request, self.template)
