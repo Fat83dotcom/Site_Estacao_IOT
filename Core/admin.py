57e3e962d2d77becd0f2c9024413a73d33eea936
fat83dotcom <fernando.quimic@gmail.com>
2024-02-13 00:53:13 -0300

    Configurando área de administração.

--- Core/admin.py
+++ Core/admin.py
@@ -1,3 +1,18 @@
+from .models import Localization, Sensor, DataSensor
 from django.contrib import admin
 
-# Register your models here.
+
+@admin.register(Localization)
+class LocalizationAdmi(admin.ModelAdmin):
+    list_display = ('id_loc', 'city', 'neighborhood')
+
+
+@admin.register(Sensor)
+class SensorAdmin(admin.ModelAdmin):
+    list_display = ('owner', 'id_sen', 'mac')
+
+
+@admin.register(DataSensor)
+class DataSensorAdmin(admin.ModelAdmin):
+    list_display = ('__str__',)
+    list_filter = ('id_sensor',)
