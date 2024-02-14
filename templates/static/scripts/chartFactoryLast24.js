3a8f385df6f1f0b4c1d3e11d6ac0f7f2047e6562
fat83dotcom <fernando.quimic@gmail.com>
2024-02-13 23:37:14 -0300

    Desenvolvimento do script auxiliar dos graficos 24 horas.

--- templates/static/scripts/chartFactoryLast24.js
+++ templates/static/scripts/chartFactoryLast24.js
@@ -1,10 +1,11 @@
-const ctx1 = document.getElementById('');
+const ctx1 = document.getElementById('tempLast24')
+
 let config1 = {
   type: 'line',
   data: {
     labels: [],
     datasets: [{
-      label: "Umidade",
+      label: "Temperatura",
       data: [],
       borderWidth: 0.5,
       fill: 'origin',
@@ -25,4 +26,61 @@ let config1 = {
     }
   }
 };
-let chartHumi = new Chart(ctx1, config1);
+let chartTemperature = new Chart(ctx1, config1)
+
+const ctx2 = document.getElementById('tempDvStdLast24')
+
+let config2 = {
+  type: 'line',
+  data: {
+    labels: [],
+    datasets: [{
+      label: "Temperatura",
+      data: [],
+      borderWidth: 0.5,
+      fill: 'origin',
+      pointRadius: 1,
+      cubicInterpolationMode: 'monotone',
+      backgroundColor: 'rgba(153, 102, 255, 0.6)',
+    }]
+  },
+
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+        ticks: {
+          callback: value => `${value} %`
+        }
+      }
+    }
+  }
+};
+let chartTempDvStdLast24 = new Chart(ctx1, config1)
+
+const updateCharts = (date, temperature) => {
+    chartTemperature.data.labels = date
+    chartTemperature.data.datasets.forEach(element => {
+      element.data = temperature
+    });
+    chartTemperature.update()
+  
+    // chartPress.data.labels = date
+    // chartPress.data.datasets.forEach(element => {
+    //   element.data = press
+    // });
+    // chartPress.update()
+  
+    // chartTemp1.data.labels = date
+    // chartTemp1.data.datasets.forEach(element => {
+    //   element.data = temp1
+    // });
+    // chartTemp1.update()
+  
+    // chartTemp2.data.labels = date
+    // chartTemp2.data.datasets.forEach(element => {
+    //   element.data = temp2
+    // });
+    // chartTemp2.update()
+}
+
