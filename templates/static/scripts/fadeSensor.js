abac9d682514220eb56cc5bd1d5ec4d4b3c1fe91
fat83dotcom <fernando.quimic@gmail.com>
2024-02-18 15:19:05 -0300

    Script que aciona os elementos ocultos no carregamento da página.

---
+++ templates/static/scripts/fadeSensor.js
@@ -0,0 +1,14 @@
+
+
+/*  Desenvolvido por: BrainStorm Tecnologia 
+    Desenvolvedor: Fernando Mendes
+    todos os direitos reservados 2024 © BrainStorm Tecnologia
+*/
+
+window.addEventListener('load', () => {
+    let contentA = document.getElementById('fade-sensor')
+
+    setTimeout(() => {
+        contentA.style.opacity = 1
+    }, 1500)
+})
