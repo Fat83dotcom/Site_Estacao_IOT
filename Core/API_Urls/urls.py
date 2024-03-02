6175986a1783dbe20e05164273afe60f0c36c3e1
fat83dotcom <fernando.quimic@gmail.com>
2024-03-02 01:54:33 -0300

    Implementando a url para para a api 7 dias.

--- Core/API_Urls/urls.py
+++ Core/API_Urls/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from Core.API.view import Graph24Hrs
+from Core.API.view import Graph24Hrs, Graph168Hrs
 
 
 urlpatterns = [
     path('data_graph_24/<int:sensor>', Graph24Hrs.as_view(), name='graph_24'),
+    path(
+        'data_graph_168/<int:sensor>', Graph168Hrs.as_view(), name='graph_168'
+    ),
 ]
