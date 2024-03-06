b4a3dee98b7bac0f5f072a43d42a0813df6efb52
fat83dotcom <fernando.quimic@gmail.com>
2024-03-05 23:11:19 -0300

    Imoortando numpy

--- Core/API/view.py
+++ Core/API/view.py
@@ -6,6 +6,7 @@ from rest_framework.response import Response
 from django.shortcuts import get_list_or_404
 from Core.Utils.classesUtils import TimeUtils
 import re
+import numpy as np
 
 
 class GraphSerializer(serializers.ModelSerializer):
