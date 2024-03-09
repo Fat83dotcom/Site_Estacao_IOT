476b7f256fb11e751a6cdd184afd91b48e409b90
fat83dotcom <fernando.quimic@gmail.com>
2024-03-09 04:09:12 -0300

    Criando os novos modulos JS que vão operar as API das estatisticas.

--- Core/tests.py
+++ Core/tests.py
@@ -1,3 +1,7 @@
-from django.test import TestCase
+# from django.test import TestCase
+from API.view import ExtractData
 
 # Create your tests here.
+
+if __name__ == '__main__':
+    print(ExtractData.dataFromDB(2, 27))
