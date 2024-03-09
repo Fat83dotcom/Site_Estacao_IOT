f3bec5fc81bc13055d9c29c83e98c33f59bbeb6b
fat83dotcom <fernando.quimic@gmail.com>
2024-03-09 04:03:12 -0300

    Nova API para calcular as estatisticas dos sensores.

--- Core/API/view.py
+++ Core/API/view.py
@@ -1,12 +1,13 @@
+import re
+import numpy as np
 from django.db.models import Q
 from Core.models import DataSensor
+from abc import ABC, abstractmethod
 from rest_framework import serializers
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.shortcuts import get_list_or_404
 from Core.Utils.classesUtils import TimeUtils
-import re
-import numpy as np
 
 
 class GraphSerializer(serializers.ModelSerializer):
@@ -28,9 +29,98 @@ class GraphSerializer(serializers.ModelSerializer):
         return data
 
 
+class StatsSerializer(serializers.Serializer):
+    _max = serializers.FloatField()
+    _min = serializers.FloatField()
+    average = serializers.FloatField()
+    stdDV = serializers.FloatField()
+
+
+class StatsFactoryInterface(ABC):
+    @abstractmethod
+    def createTemperatureStats(self): pass
+
+    @abstractmethod
+    def createHUmidityStats(self): pass
+
+    @abstractmethod
+    def createPressureStats(self): pass
+
+
+class ConcreteStats(StatsFactoryInterface):
+    def createTemperatureStats(
+        self, _max: float, _min: float, average: float, stdDV: float
+    ):
+        return Stats(_max, _min, average, stdDV)
+
+    def createHUmidityStats(
+        self, _max: float, _min: float, average: float, stdDV: float
+    ):
+        return Stats(_max, _min, average, stdDV)
+
+    def createPressureStats(
+        self, _max: float, _min: float, average: float, stdDV: float
+    ):
+        return Stats(_max, _min, average, stdDV)
+
+
+class Stats:
+    def __init__(
+        self, _max: float, _min: float, average: float, stdDV: float
+    ) -> None:
+        self._max = _max
+        self._min = _min
+        self.average = average
+        self.stdDV = stdDV
+
+
+class ExtractData:
+    def __init__(self, querySet) -> None:
+        self.__temperature = []
+        self.__humidity = []
+        self.__pressure = []
+        for data in querySet:
+            self.__temperature.append(data.temperature)
+            self.__humidity.append(data.humidity)
+            self.__pressure.append(data.pressure)
+
+    def getArrayTemp(self) -> np.array:
+        return np.array(self.__temperature)
+
+    def getArrayHumi(self) -> np.array:
+        return np.array(self.__humidity)
+
+    def getArrayPress(self) -> np.array:
+        return np.array(self.__pressure)
+
+
+class Max:
+    @classmethod
+    def getMax(self, array):
+        return round(np.max(array))
+
+
+class Min:
+    @classmethod
+    def getMin(self, array) -> float:
+        return round(np.min(array), 2)
+
+
+class Average:
+    @classmethod
+    def getAverage(self, array) -> float:
+        return round(np.average(array), 2)
+
+
+class StdDeviation:
+    @classmethod
+    def getStdDeviaton(self, array) -> float:
+        return round(np.std(array), 2)
+
+
 class QueryData:
     @classmethod
-    def query(self, sensor, model, time: int):
+    def contextQuery(self, sensor, model, time: int):
         return model.objects.filter(
             Q(id_sensor=sensor) & Q(
                 date_hour__range=(
@@ -43,8 +133,8 @@ class QueryData:
 class Graph24Hrs(APIView):
     def get(self, request, *args, **kwargs):
         dataSensor = get_list_or_404(
-            QueryData.query(
-                sensor=kwargs.get('sensor'), model=DataSensor, time=24
+            QueryData.contextQuery(
+                sensor=kwargs.get('sensor'), model=DataSensor, time=27
             )[:1440]
         )
         serializer = GraphSerializer(
@@ -57,8 +147,8 @@ class Graph24Hrs(APIView):
 class Graph168Hrs(APIView):
     def get(self, request, *args, **kwargs):
         dataSensor = get_list_or_404(
-            QueryData.query(
-                sensor=kwargs.get('sensor'), model=DataSensor, time=168
+            QueryData.contextQuery(
+                sensor=kwargs.get('sensor'), model=DataSensor, time=171
             )[:10080]
         )
         serializer = GraphSerializer(
@@ -71,7 +161,7 @@ class Graph168Hrs(APIView):
 class Graph720Hrs(APIView):
     def get(self, request, *args, **kwargs):
         dataSensor = get_list_or_404(
-            QueryData.query(
+            QueryData.contextQuery(
                 sensor=kwargs.get('sensor'), model=DataSensor, time=720
             )[:10080]
         )
@@ -88,7 +178,44 @@ class ScatterGraph_1Hr(APIView):
 
 
 class Stats24Hrs(APIView):
-    pass
+    hours: int = 27
+
+    def get(self, *args, **kwargs):
+        sensor = kwargs.get('sensor')
+        querySet = QueryData.contextQuery(
+            sensor, DataSensor, self.hours
+        )
+        extract = ExtractData(querySet)
+        arrayTemp = extract.getArrayTemp()
+        arrayHumi = extract.getArrayHumi()
+        arrayPress = extract.getArrayPress()
+
+        statsFactory = ConcreteStats()
+
+        tempStats = statsFactory.createTemperatureStats(
+            Max.getMax(arrayTemp),
+            Min.getMin(arrayTemp),
+            Average.getAverage(arrayTemp),
+            StdDeviation.getStdDeviaton(arrayTemp)
+        )
+        humiStats = statsFactory.createHUmidityStats(
+            Max.getMax(arrayHumi),
+            Min.getMin(arrayHumi),
+            Average.getAverage(arrayHumi),
+            StdDeviation.getStdDeviaton(arrayHumi)
+        )
+        pressStats = statsFactory.createHUmidityStats(
+            Max.getMax(arrayPress),
+            Min.getMin(arrayPress),
+            Average.getAverage(arrayPress),
+            StdDeviation.getStdDeviaton(arrayPress)
+        )
+
+        serializer = StatsSerializer(
+            instance=[tempStats, humiStats, pressStats],
+            many=True
+        )
+        return Response(serializer.data)
 
 
 class Stats168Hrs(APIView):
