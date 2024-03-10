dad54b60fdf7d16ae0919ee726f1f9327aa51de3
fat83dotcom <fernando.quimic@gmail.com>
2024-03-10 17:20:53 -0300

    Retirando funções que não serão usadas e reunidos novas funçoes comuns.

--- templates/static/scripts/comonFunctions.js
+++ templates/static/scripts/comonFunctions.js
@@ -45,36 +45,67 @@ const graphConfigFactory = (ticks) => {
     };
 };
 
-const max = dataArray => {
-    if (dataArray.length === 0) {
-        return undefined
+const fillerStdAverange = (object, arrayTarget, average, stdDeviation) => {
+    let stdUp = parseFloat(average) + parseFloat(stdDeviation)
+    let stdDown = parseFloat(average) - parseFloat(stdDeviation)
+
+    const configStdAverange = [
+        {
+            label: 'Desvio Padrão Superior',
+            data: Array(arrayTarget.length).fill(stdUp),
+            borderColor: 'green',
+            borderWidth: 1,
+            fill: {
+                target: '2',
+            },
+            backgroundColor: 'rgb(79, 232, 95, 0.4)',
+            borderDash: [5, 5],
+            pointStyle: 'dash',
+        },
+        {
+            label: 'Média',
+            data: Array(arrayTarget.length).fill(average),
+            borderColor: 'red',
+            borderWidth: 1,
+            fill: false,
+            borderDash: [5, 5],
+            pointStyle: 'dash',
+        },
+        {
+            label: 'Desvio Padrão Inferior',
+            data: Array(arrayTarget.length).fill(stdDown),
+            borderColor: 'green',
+            borderWidth: 1,
+            fill: {
+                target: '-1',
+            },
+            borderDash: [5, 5],
+            backgroundColor: 'rgb(79, 232, 95, 0.4)',
+            pointStyle: 'dash',
+        }
+    ]
+    configStdAverange.forEach((element) => {
+        object.data.datasets.push(element)
+    })
+};
+
+const updateChartStdDeviation = (config, chart, _label, date, humidity, aver, stdD) => {
+    if (config.data.datasets.length > 0) {
+        config.data.datasets.length = 0
     }
-    let max
-    dataArray.forEach((element, index) => {
-        if (index === 0) max = element
-        if (element > max) max = element
-    });
-    return max
-};
-
-const min = dataArray => {
-    if (dataArray.length === 0) {
-        return undefined
-    }
-    let min
-    dataArray.forEach((element, index) => {
-        if (index === 0) min = element
-        if (element < min) min = element
-    });
-    return min
-};
-
-const average = dataArray => {
-    return (dataArray.reduce((a, b) => a + b, 0) / dataArray.length)
-};
-
-const stdDeviation = (dataArray, average) => {
-    return Math.sqrt(
-        dataArray.reduce((acc, val) => acc + Math.pow(val - average, 2), 0
-        ) / dataArray.length)
+    config.data.labels = date
+    config.data.datasets.push(
+        {
+            label: _label,
+            data: humidity,
+            borderWidth: 0.5,
+            pointRadius: 1.5,
+            fill: false,
+            cubicInterpolationMode: 'monotone',
+            backgroundColor: 'rgb(35, 35, 35)',
+            radius: 1,
+        }
+    )
+    fillerStdAverange(config, date, aver, stdD)
+    chart.update()
 };
